db18d77058a2d52d1a452003857329e92ce1ee4b
gianmartind <63358478+gianmartind@users.noreply.github.com>
2021-11-01 15:24:34 +0700

    fix many

--- detect_image.py
+++ detect_image.py
@@ -8,9 +8,7 @@ from keras_retinanet.utils.visualization import draw_box, draw_caption
 from keras_retinanet.utils.colors import label_color
 
 # import miscellaneous modules
-import matplotlib.pyplot as plt
 import cv2
-import os
 import numpy as np
 import time
 
@@ -18,31 +16,82 @@ import time
 import tensorflow as tf
 
 from PIL import Image
+import string
+import random
 
 def get_session():
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     return tf.compat.v1.Session(config=config)
 
+
 def init_tf():
     tf.compat.v1.keras.backend.set_session(get_session())
+
 
 def get_model(model_path):
     print("Loading Model: {}".format(model_path))
     model = models.load_model(model_path, backbone_name='resnet50')
 
-    #Check that it's been converted to an inference model
+    # Check that it's been converted to an inference model
     try:
         model = models.convert_model(model)
     except:
         print("Model is likely already an inference model")
-    
+
     return model
+
+
+def draw_text(img, text,
+              font=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+              pos=(0, 0),
+              font_scale=1,
+              font_thickness=1,
+              text_color=(255, 255, 255),
+              text_color_bg=(0, 0, 0)
+              ):
+
+    x, y = pos
+    text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+    text_w, text_h = text_size
+    cv2.rectangle(img, pos, (x + text_w, y + text_h), text_color_bg, -1)
+    cv2.putText(img, text, (x, y + text_h + font_scale - 1),
+                font, font_scale, text_color, font_thickness)
+
+    return text_size
+
+
+def compute_iou(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = abs(max((xB - xA, 0)) * max((yB - yA), 0))
+    if interArea == 0:
+        return 0
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = abs((boxA[2] - boxA[0]) * (boxA[3] - boxA[1]))
+    boxBArea = abs((boxB[2] - boxB[0]) * (boxB[3] - boxB[1]))
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
+
+def id_generator(size=12, chars=string.ascii_letters + string.digits):
+   return ''.join(random.choice(chars) for _ in range(size))
 
 def detect(image_path, model, filename, confidence_cutoff):
     image = np.asarray(Image.open(image_path).convert('RGB'))
     image = image[:, :, ::-1].copy()
-    labels_to_names = {0: 'motor', 1: 'mobil'}
+    labels_to_names = {0: 'motor', 1: 'mobil', 2: 'sedan'}
 
     # copy to draw on
     draw = image.copy()
@@ -55,25 +104,40 @@ def detect(image_path, model, filename, confidence_cutoff):
     # Run the inference
     start = time.time()
 
-    boxes, scores, labels = model.predict_on_batch(np.expand_dims(image2, axis=0))
+    boxes, scores, labels = model.predict_on_batch(
+        np.expand_dims(image2, axis=0))
     print("processing time: ", time.time() - start)
 
     # correct for image scale
     boxes /= scale
 
-    detect_result = {"mobil":0, "motor":0}
+    detect_result = {"mobil": 0, "motor": 0, "sedan": 0}
+
+    cutoff_motor = confidence_cutoff['motor']
+    cutoff_mobil = confidence_cutoff['mobil']
+    cutoff_sedan = confidence_cutoff['sedan']
 
     # visualize detections
     for box, score, label in zip(boxes[0], scores[0], labels[0]):
-        # scores are sorted so we can break
-        if score < confidence_cutoff:
-            break
+        # if overlap break
 
-        #Add boxes and captions
+        # scores are sorted so we can break
+        if label == 0:
+            if score < cutoff_motor:
+                break
+        elif label == 1:
+            if score < cutoff_mobil:
+                break
+        elif label == 2:
+            if score < cutoff_sedan:
+                break
+
+        # Add boxes and captions
         color = (255, 255, 255)
         thickness = 2
         b = np.array(box).astype(int)
-        cv2.rectangle(draw, (b[0], b[1]), (b[2], b[3]), color, thickness, cv2.LINE_AA)
+        cv2.rectangle(draw, (b[0], b[1]), (b[2], b[3]),
+                      color, thickness, cv2.LINE_AA)
 
         if(label > len(labels_to_names)):
             print("WARNING: Got unknown label, using 'detection' instead")
@@ -81,14 +145,16 @@ def detect(image_path, model, filename, confidence_cutoff):
         else:
             caption = "{} {:.3f}".format(labels_to_names[label], score)
 
-        detect_result[labels_to_names[label]] = detect_result[labels_to_names[label]] + 1
+        detect_result[labels_to_names[label]
+                      ] = detect_result[labels_to_names[label]] + 1
 
         #cv2.putText(draw, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 3)
-        cv2.putText(draw, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        #cv2.putText(draw, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+        draw_text(draw, caption, pos=(b[0], b[1] - 10))
 
-    #Write out image
+    # Write out image
     draw = Image.fromarray(draw)
-    detected_image_path = './static/uploads/detection/' + filename
+    detected_image_path = './static/uploads/detection/' + id_generator() + filename
     draw = draw.save(detected_image_path)
 
     detect_result['path'] = detected_image_path
--- main.py
+++ main.py
@@ -8,17 +8,8 @@ from matplotlib.figure import Figure
 
 from IPython import get_ipython
 
-import string
-import random
-'''
-import keras
+from os import listdir
 
-# import keras_retinanet
-from keras_retinanet import models
-from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
-from keras_retinanet.utils.visualization import draw_box, draw_caption
-from keras_retinanet.utils.colors import label_color
-'''
 import detect_image
 # import miscellaneous modules
 import matplotlib.pyplot as plt
@@ -34,37 +25,48 @@ app = Flask(__name__, template_folder='template')
 app.config['UPLOAD_FOLDER'] = './static/uploads'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    models = [f for f in listdir('./static/models')]
 
-@app.route('/upload', methods = ['GET', 'POST'])
+    return render_template('index.html', models=models)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     global filename
     if request.method == 'POST':
         f = request.files['file']
-        save_dir = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename))
+        save_dir = os.path.join(
+            app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
         filename = f.filename
         f.save(save_dir)
-        return jsonify({"result":save_dir})
+        return jsonify({"result": save_dir})
 
 model = ""
 filename = ""
 
+@app.route('/get_model', methods=['GET', 'POST'])
+def get_model():
+    global model
+    model_name = request.args.get("model_name")
+    model_path = './static/models/'+model_name
+    model = detect_image.get_model(model_path)
+    return jsonify({"result": model_name})
+
 @app.route('/detect')
 def detect_object():
     image_path = request.args.get('image_path')
-    confidence_cutoff = 0.7
+    confidence_cutoff = {'mobil': 0.7, 'motor': 0.7, 'sedan': 0.7}
     print(image_path)
-    detected_result = detect_image.detect(image_path, model, filename, confidence_cutoff)
+    detected_result = detect_image.detect(
+        image_path, model, filename, confidence_cutoff)
 
     return jsonify(detected_result)
 
+
 if __name__ == '__main__':
     detect_image.init_tf()
-
-    # load retinanet model
-    model_path = r'E:\Kuliah\Proyek Data Science\#_PROYEK\Retinanet-Tutorial\keras-retinanet\Dataset_2\snapshots\resnet50_pascal_15.h5'
-    model = detect_image.get_model(model_path)
 
     app.run(host='0.0.0.0', port='80')
